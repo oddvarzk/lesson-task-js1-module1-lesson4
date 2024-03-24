48c159772c431d853b571d34c6d04a932dd336a9
oddvarzk <122876488+oddvarzk@users.noreply.github.com>
2024-03-24 18:29:54 +0100

    Update script.js
    Finished

--- js/script.js
+++ js/script.js
@@ -13,3 +13,70 @@ const games = [
         released: 2020
     }
 ];
+
+function printSeason(season){
+
+console.log(season);
+
+}
+
+printSeason("summer");
+
+function printNumbers(firstNumber, secondNumber){
+    console.log(firstNumber, secondNumber);
+
+}
+printNumbers(15, 21);
+
+function addNumbers (numberOne, numberTwo, numberThree){
+    const result = numberOne + numberTwo + numberThree; 
+    return result;
+}
+const total = addNumbers(5, 10, 15);
+
+const totalContainer = document.querySelector(".total");
+totalContainer.innerHTML = total;
+
+function createGreeting(name){
+    const greeting = "Hello, my name is" + name;
+    return greeting;
+}
+
+const greeting = createGreeting("Oddvardo");
+
+const nameContainer =  document.querySelector("#name");
+nameContainer.innerHTML = greeting;
+
+function printListItems (items){
+    for(let i =0;  i < items.length; i++){
+        console.log(items[i]);
+    }
+}
+printListItems(sports);
+
+
+function createGames (gamesList){
+
+    let html = "";
+
+    for(let i = 0; i < gamesList.length; i++){
+
+        let releaseYear = "Unknown year";
+
+        if (gamesList[i].released){
+            releaseYear = gamesList[i].released;
+        }
+
+        html += `<div class="game">
+                    <h5>${gamesList[i].name}</h5>
+                    <P>Released: ${releaseYear}</p>     
+                </div>
+                `
+    }
+    return html;
+}
+
+const newHTML = createGames(games);
+const gameContainer = document.querySelector(".game-container");
+
+gameContainer.innerHTML = newHTML;
